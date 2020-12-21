22f5c455c95800ea1f407f0871b8dc93c79bd8e7
Daniel Scarpim <scarpim@architrave.de>
2020-12-21 13:26:11 +0100

    Parse csv file

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -1,9 +1,19 @@
 /* eslint-disable import/prefer-default-export */
+import Papa from 'papaparse';
+
 export const fetchMetrics = async () => {
   try {
-    const response = await fetch('api/data.csv');
-    const metrics = response.body;
-    console.log('metrics: ', metrics);
+    let response = null;
+    Papa.parse('api/data.csv', {
+      download: true,
+      header: true,
+      delimiter: ',',
+      complete: (results) => {
+        response = results.data;
+        console.log('response: ', response);
+      },
+    });
+    const metrics = response;
     return metrics;
   } catch (error) {
     throw new Error(error);
--- src/components/Dashboard/dashboard.tsx
+++ src/components/Dashboard/dashboard.tsx
@@ -3,6 +3,7 @@ import useDashboard from './useDashboard';
 
 const Dashboard = () => {
   const { metrics } = useDashboard();
+  console.log('metrics: ', metrics);
   return <div>dashboard</div>;
 };
 
